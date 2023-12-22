171421b379be9bc1cee5be0a29c66c03fa880629
himza25 <himza.of.theking95@gmail.com>
2023-12-22 11:10:43 -0800

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,30 @@
-# install flask from pip3.
+# Puppet manifest to ensure Python, pip, and Werkzeug 2.1.1 are installed before Flask 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  }
+# Ensure Python is installed
+package { 'python3':
+  ensure => installed,
+}
+
+# Ensure pip is installed
+exec { 'install-pip':
+  command => 'apt-get update && apt-get install -y python3-pip',
+  unless  => 'which pip3',
+  require => Package['python3'], # This makes sure Python is installed first
+  path    => ['/usr/bin', '/usr/local/bin'],
+}
+
+# Ensure Werkzeug 2.1.1 is installed
+exec { 'install-werkzeug':
+  command => 'pip3 install Werkzeug==2.1.1',
+  unless  => 'pip3 freeze | grep Werkzeug==2.1.1',
+  require => Exec['install-pip'], # This ensures pip is installed first
+  path    => ['/usr/bin', '/usr/local/bin'],
+}
+
+# Install Flask 2.1.0
+exec { 'install-flask':
+  command => 'pip3 install Flask==2.1.0',
+  unless  => 'pip3 freeze | grep Flask==2.1.0',
+  require => [Exec['install-pip'], Exec['install-werkzeug']], # Ensures pip and Werkzeug are installed first
+  path    => ['/usr/bin', '/usr/local/bin'],
+}
