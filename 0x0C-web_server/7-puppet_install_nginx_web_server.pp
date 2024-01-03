d9e986fa96a31fdae053887fc103c2f63faa7d20
himza25 <himza.of.theking95@gmail.com>
2024-01-03 12:36:17 -0800

    puppet task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,33 @@
-# Puppet manifest to install and configure Nginx
-class nginx_setup {
-    # Ensure Nginx is installed
-    package { 'nginx':
-        ensure => installed,
-    }
-
-    # Start and enable Nginx service
-    service { 'nginx':
-        ensure     => running,
-        enable     => true,
-        require    => Package['nginx'],
-        subscribe  => File['/etc/nginx/sites-available/default'],
-    }
-
-    # Configure Nginx to serve 'Hello World!' page at root
-    file { '/var/www/html/index.html':
-        ensure  => file,
-        content => '<html><body><h1>Hello World!</h1></body></html>',
-        require => Package['nginx'],
-    }
-
-    # Configure Nginx to redirect /redirect_me to a different URL with 301
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => template('nginx/default.erb'),
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
+# install nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_setup
+# website index file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# redirect_me config
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => '
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+            # Am omar but you can call me dehao
+        }',
+}
+
+# stop nginx
+exec { 'stop service':
+  command => 'sudo service nginx stop',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
+# run nginx
+exec { 'start service':
+  command => 'sudo service nginx start',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
