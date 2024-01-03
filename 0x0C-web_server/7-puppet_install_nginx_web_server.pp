3c768877a0b433876c04983b957542c85fe301f0
himza25 <himza.of.theking95@gmail.com>
2024-01-03 12:41:10 -0800

    puppet task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,31 @@
-# install nginx
+# Puppet Manifest for Nginx Setup
+
+# Ensure Nginx is installed on the system
 package { 'nginx':
   ensure => installed,
 }
 
-# website index file
+# Create and manage the content of the index.html file
 file { '/var/www/html/index.html':
+  ensure  => present,
   content => 'Hello World!',
+  require => Package['nginx'], # Ensure Nginx is installed before modifying files
 }
 
-# redirect_me config
-file_line { 'redirect_me':
-  ensure => present,
+# Configure the Nginx to perform 301 redirect for '/redirect_me'
+file_line { 'nginx_redirect_me':
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => '
-        location /redirect_me {
-            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            # Am omar but you can call me dehao
-        }',
+  line   => '        location /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }',
+  match  => '^        location /redirect_me',
+  ensure => present,
+  require => Package['nginx'], # Depend on Nginx installation
+  notify => Service['nginx'],  # Notify Nginx service to restart after change
 }
 
-# stop nginx
-exec { 'stop service':
-  command => 'sudo service nginx stop',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-}
-
-# run nginx
-exec { 'start service':
-  command => 'sudo service nginx start',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+# Manage the Nginx service, ensuring it is running
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => File['/var/www/html/index.html'], # Ensure file is in place before service starts
+  subscribe => File_Line['nginx_redirect_me'], # Restart Nginx if redirect configuration changes
 }
