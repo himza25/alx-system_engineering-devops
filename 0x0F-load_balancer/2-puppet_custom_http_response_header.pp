0c58a01ab677f15b3b9991753c00f64861ca9bac
himza25 <himza.of.theking95@gmail.com>
2024-01-08 18:08:08 -0800

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,38 @@
-# Installs a Nginx server with custom HTTP header
+# Puppet script to set up Nginx with a custom HTTP header
 
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+class nginx_setup {
+
+  exec { 'update_packages':
+    command  => '/usr/bin/apt-get -y update',
+    path     => '/usr/bin:/bin',
+    logoutput => true,
+    before   => Exec['install_nginx'],
+  }
+
+  exec { 'install_nginx':
+    command  => '/usr/bin/apt-get -y install nginx',
+    path     => '/usr/bin:/bin',
+    logoutput => true,
+    unless   => '/usr/bin/dpkg -l | /bin/grep nginx',
+    require  => Exec['update_packages'],
+    before   => Exec['configure_nginx'],
+  }
+
+  exec { 'configure_nginx':
+    command  => "sed -i 's/include \\/etc\\/nginx\\/sites-enabled\\/*;/include \\/etc\\/nginx\\/sites-enabled\\/*;\\n\\tadd_header X-Served-By \"\$HOSTNAME\";/' /etc/nginx/nginx.conf",
+    path     => '/usr/bin:/bin',
+    logoutput => true,
+    unless   => "grep -q 'X-Served-By' /etc/nginx/nginx.conf",
+    require  => Exec['install_nginx'],
+    notify   => Service['nginx_service'],
+  }
+
+  service { 'nginx_service':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    subscribe => Exec['configure_nginx'],
+  }
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
-
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
-}
-
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-}
+include nginx_setup
