a63c606a444787da676ab7d13595e60a141016a7
himza25 <himza.of.theking95@gmail.com>
2024-02-14 10:45:07 -0800

    update tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,20 @@
-# A puppet manuscript to replace a line in a file on a server
+# 0-strace_is_your_friend.pp
+# Puppet manifest to debug and fix a 500 error in a Wordpress installation
 
-$file_to_edit = '/var/www/html/wp-settings.php'
+$file_path = '/var/www/html/wp-config.php'
 
-#replace line containing "phpp" with "php"
+# Ensure the wp-config.php file exists
+if $::kernel == 'Linux' and $::operatingsystem == 'Ubuntu' {
+  file { $file_path:
+    ensure  => file,
+    require => Package['wordpress'],
+  }
 
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+  # Hypothetical fix: Correct a typo or misconfiguration detected by strace
+  # Replace 'DB_HOST', 'localhostt' with 'DB_HOST', 'localhost'
+  exec { 'correct_db_host':
+    command => "sed -i 's/DB_HOST', 'localhostt'/DB_HOST', 'localhost'/g' ${file_path}",
+    path    => ['/bin', '/usr/bin'],
+    onlyif  => "grep -q 'DB_HOST', 'localhostt' ${file_path}",
+  }
 }
