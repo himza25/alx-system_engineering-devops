bd29e43d18b19f491ce48ff90c0d36b85d9bb3ed
himza25 <himza.of.theking95@gmail.com>
2024-02-13 18:50:57 -0800

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Module to recursively query Reddit API for all hot article titles of a subreddit."""
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Recursively collects titles of all hot articles for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'MyRedditRecursiveScraper/1.0 (by /u/yourRedditUsername)'}
-    params = {'limit': 100, 'after': after}  # Use pagination
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+def recurse(subreddit, hot_list=[]):
+    """returning the classement of the top ten post titles"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    if response.status_code != 200:
-        return None
-
-    try:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            hot_list.append(post['data']['title'])
-
-        after = data['data'].get('after')  # Use .get to safely handle absence of 'after'
-        if after is not None:
-            return recurse(subreddit, hot_list, after)
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
         return hot_list
-    except ValueError:
-        # Catch JSON decode error
-        return None
+    else:
+        return (None)
