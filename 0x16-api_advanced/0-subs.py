a8644bdbf748199b92152d3c4c66818caacdb915
himza25 <himza.of.theking95@gmail.com>
2024-02-13 18:57:08 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,16 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/himza25)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200 and 'application/json' in response.headers['Content-Type']:
+        try:
+            results = response.json().get("data")
+            return results.get("subscribers", 0)
+        except ValueError:  # Includes json.decoder.JSONDecodeError
+            print("Error decoding JSON")
+            return 0
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
